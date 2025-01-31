01ee731aa8b07d6fcf7267214fcb5a86e0fb2a52
Seung(emily_the_dev) <emily101304@naver.com(emily101304)>
2025-01-31 17:53:00 +0900

    sql 주석값 수정

--- schedule.sql
+++ schedule.sql
@@ -1,10 +1,8 @@
---CREATE TABLE schedule (
---                            schedule_id BIGINT AUTO_INCREMENT PRIMARY KEY,     -- Schedule ID, 자동으로 증가하는 기본 키
---                            user_id BIGINT AUTO_INCREMENT NOT NULL,      -- 자동으로 증가하는 키,
---                            todo VARCHAR NOT NULL,         -- 할 일, NULL을 허용하지 않음
---                            password VARCHAR NOT NULL,          -- 비밀번호, NULL을 허용하지 않음
---                            inti_date TIMESTAMP,                      -- 작성일
---                            mod_date TIMESTAMP                -- 입사일
--- );
-
--- Show table;
+CREATE TABLE schedule (
+                           schedule_id BIGINT AUTO_INCREMENT PRIMARY KEY,     -- Schedule ID, 자동으로 증가하는 기본 키
+                           user_id BIGINT AUTO_INCREMENT NOT NULL,      -- User ID, 자동으로 증가하는 키,
+                           todo VARCHAR NOT NULL,         -- 할 일, NULL을 허용하지 않음
+                           password VARCHAR NOT NULL,          -- 비밀번호, NULL을 허용하지 않음
+                           inti_date TIMESTAMP,                      -- 작성일
+                           mod_date TIMESTAMP                -- 입사일
+);
